708fb394722ce6d349438e0b1a9e1655a5c6f9d9
MicahZheng <micah.com.cn@gmail.com>
2019-04-18 13:14:06 +0800

    orm修改   models添加

--- www/orm.py
+++ www/orm.py
@@ -61,7 +61,7 @@ async def execute(sql, args, autocommit=True):
         return affected
 
 
-async def create_arg_string(num):
+def create_arg_string(num):
     L = []
     for i in range(num):
         L.append('?')
@@ -82,7 +82,7 @@ class Field(object):
 
 class StringField(Field):
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
-        super.__init__(name, ddl, primary_key, default)
+        super().__init__(name, ddl, primary_key, default)
 
 
 class IntegerField(Field):
@@ -92,7 +92,7 @@ class IntegerField(Field):
 
 class BooleanField(Field):
     def __init__(self, name=None, default=False):
-        super().__init__(self, name, 'boolean', False, default)
+        super().__init__(name, 'boolean', False, default)
 
 
 class FloatField(Field):
@@ -111,33 +111,33 @@ class ModelMetaclass(type):
             return type.__new__(mcs, name, bases, attrs)
         tableName = attrs.get('__table__', None) or name
         logging.info('found model:%s (table:%s)' % (name, tableName))
-        mapping = dict()
+        mappings = dict()
         fields = []
-        primary_key = None
+        primaryKey = None
         for k, v in attrs.items():
             if isinstance(v, Field):
                 logging.info('found mapping: %s ==> %s' % (k, v))
-                mapping[k] = v
+                mappings[k] = v
                 if v.primary_key:
-                    if primary_key:
-                        raise StandardError('Duplicate primary key for field: %s' % k)
+                    if primaryKey:
+                        raise Exception('Duplicate primary key for field: %s' % k)
                     primaryKey = k
                 else:
                     fields.append(k)
-        if not primary_key:
-            raise StandardError('primary_key not found')
+        if not primaryKey:
+            raise Exception('primary_key not found')
 
-        for k in mapping.keys():
+        for k in mappings.keys():
             attrs.pop(k)
 
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
-        attrs['__mapping__'] = mapping
+        attrs['__mappings__'] = mappings
         attrs['__table__'] = tableName
-        attrs['__primary_key__'] = primary_key
+        attrs['__primary_key__'] = primaryKey
         attrs['__fields__'] = fields
-        attrs['__select__'] = 'select `%s`, %s from %s' % (primary_key, ','.join(escaped_fields), tableName)
-        attrs['__insert__'] = 'insert into %s (%s, `%s`) values (%s)' % (tableName, ','.join(escaped_fields), primary_key, create_arg_string(len(escaped_fields) + 1))
-        attrs['__update__'] = 'update %s set %s where `%s`= ?' % (tableName, ','.join(map(lambda f: '`%s` = ?' % (mapping.get(k).name or f)), fields), primaryKey)
+        attrs['__select__'] = 'select `%s`, %s from %s' % (primaryKey, ','.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into %s (%s, `%s`) values (%s)' % (tableName, ','.join(escaped_fields), primaryKey, create_arg_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update %s set %s where `%s`= ?' % (tableName, ','.join(map(lambda f: '`%s` = ?' % (mappings.get(k).name or f), fields)), primaryKey)
         attrs['__delete__'] = 'delete from %s where `%s` = ?' % (tableName, primaryKey)
         return type.__new__(mcs, name, bases, attrs)
 
